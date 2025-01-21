563ce1310926be0af97e768aa9cf9eef3960c88d
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-01-21 21:11:42 +0530

    project structire change

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,19 +1,36 @@
 import { useState, useEffect } from "react";
 
 export default function Profile({ image, name, alt }) {
-  let [buttonState, setButtonState] = useState(true);
+  let [buttonText, setButtonText] = useState("hide");
+  let [imageVisible, setImageVisible] = useState(false);
+
   useEffect(() => {
-    let image = document.querySelector("img");
-    if (!buttonState) {
-      console.log("name", name);
-      console.log("image", image);
-      image.style.height = "0px";
-    }
-  }, [buttonState]);
+    // This runs for change in button text. Checks for below condition every time button text changes
+    if (imageVisible) setImageVisible(false);
+    else setImageVisible(true);
+  }, [buttonText]);
+
+  useEffect(() => {
+    // This sets images to SHOW by default
+    setImageVisible(true);
+  }, []);
+
   return (
-    <li className="profile">
-      <button onClick={(e) => setButtonState(!buttonState)}>
-        {buttonState ? "Hide Image" : "Show Image"}
+    <li
+      className={
+        imageVisible ? "profile-visible profile" : "profile-hidden profile"
+      }
+    >
+      <button
+        onClick={(e) => {
+          if (buttonText === "hide") {
+            setButtonText("show");
+          } else {
+            setButtonText("hide");
+          }
+        }}
+      >
+        {buttonText === "hide" ? "Hide" : "Show"}
       </button>
       <h2>{name}</h2>
       <img src={image} alt={alt}></img>
